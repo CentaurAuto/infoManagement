62522b70d53fcaeabf3895de3ed5df01b4de04e4
rameshkonatala <konatalaramesh@gmail.com>
2016-12-04 17:35:13 +0530

    linked vehicles to supplier data for checking system defects according to batches

--- mysite/bikeHome/views.py
+++ mysite/bikeHome/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,render_to_response
 from django.http import HttpResponseRedirect
-from .models import Vehicle,Supplier,System
+from .models import Vehicle,Supplier,System,Batch,SubSystem
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
@@ -54,7 +54,23 @@ def requestPage(request,vehicle_id):
 
 def supplierIndex(request):
 	supplier_list=Supplier.objects.all()
-	context={'supplier_list':supplier_list,}
+	batches_list=[]
+	systems_list=[]
+	for supplier in supplier_list:
+		batches=Batch.objects.filter(supplier=supplier)
+		batches_list.append(batches)
+		systems_list.append([])
+		for batch in batches:
+			batch_system=batch.system
+			vehicles=Vehicle.objects.filter(batch=batch)
+			systems_list[len(systems_list)-1].append([batch_system,len(vehicles),0])
+			for vehicle in vehicles:
+				system_status=[vehicle.system_1(),vehicle.system_2(),vehicle.system_3(),vehicle.system_4(),vehicle.system_5(),vehicle.system_6(),vehicle.system_7(),vehicle.system_8()]
+				system_name=batch_system.system_name
+				if not system_status[int(system_name[len(system_name)-1])-1]:
+					systems_list[-1][-1][2]+=1
+	main_list=zip(supplier_list,batches_list,systems_list)
+	context={'supplier_list':supplier_list,'main_list':main_list}
 	return render(request,'bikeHome/indexSupplier.html',context)
 
 def supplierDashboard(request,supplier_id):
